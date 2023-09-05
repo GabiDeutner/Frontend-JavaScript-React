f5b06d04fc37b34ad0d524318f3a4bbdc6a0e58e
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-05 17:14:09 -0300

    Update Exercicio_tipo_de_dados.js

--- 1.Curso_JavaScript/2.Tipo_De_Dados/Exercicio_tipo_de_dados.js
+++ 1.Curso_JavaScript/2.Tipo_De_Dados/Exercicio_tipo_de_dados.js
@@ -5,13 +5,29 @@ e números não levam aspas.
 
 // Declare uma variável contendo uma string
 
+var nome = "Gabi";
+
 // Declare uma variável contendo um número dentro de uma string
 
+var idade = "29";
+
 // Declare uma variável com a sua idade
+
+var idade2 = 29;
 
 // Declare duas variáveis, uma com seu nome
 // e outra com seu sobrenome e some as mesmas
 
+var meuNome = "Gabi";
+var sobrenome = "Deutner";
+var nomeCompleto = meuNome + ' ' + sobrenome;
+
 // Coloque a seguinte frase em uma variável: It's time
 
+var frase = "It's time";
+
 // Verifique o tipo da variável que contém o seu nome
+
+console.log(typeof nome);
+
+console.log(nome,idade, idade2, nomeCompleto, frase);
