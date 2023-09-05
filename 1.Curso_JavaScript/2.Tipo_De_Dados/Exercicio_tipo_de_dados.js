1a4352a12d2f69a0e86e675995bda14226644776
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-05 17:11:00 -0300

    Create Exercicio_tipo_de_dados.js

---
+++ 1.Curso_JavaScript/2.Tipo_De_Dados/Exercicio_tipo_de_dados.js
@@ -0,0 +1,17 @@
+/*EXERCÍCIO
+Dica rápida, frases (string) devem ser colocadas entre aspas (simples ou duplas) 
+e números não levam aspas.
+*/
+
+// Declare uma variável contendo uma string
+
+// Declare uma variável contendo um número dentro de uma string
+
+// Declare uma variável com a sua idade
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+// Coloque a seguinte frase em uma variável: It's time
+
+// Verifique o tipo da variável que contém o seu nome
