89bedc15a6085737a40809317fb858168febadc4
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-05 19:50:07 -0300

    Create Exercicio_Funcoes.js

---
+++ 5.Funcoes/Exercicio_Funcoes.js
@@ -0,0 +1,31 @@
+// EXERCÍCIO
+
+// Crie uma função para verificar se um valor é Truthy
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+
+// Crie uma função que verifica se um número é par
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
+
+// Corrija o erro abaixo
+function precisoVisitar(paisesVisitados) {
+    var totalPaises = 193;
+    return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+  }
+  function jaVisitei(paisesVisitados) {
+    return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+  }
+  precisoVisitar(20);
+  jaVisitei(20);
+  
