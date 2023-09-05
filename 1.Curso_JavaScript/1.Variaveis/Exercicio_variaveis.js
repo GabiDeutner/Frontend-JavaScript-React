1617e02d14c4a708d5166be143d5ddfe0760969d
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-05 16:42:21 -0300

    Create Exercicio_variaveis.js

---
+++ 1.Curso_JavaScript/1.Variaveis/Exercicio_variaveis.js
@@ -0,0 +1,30 @@
+//EXERCÍCIO
+/*Dica rápida, frases (string) devem ser colocadas entre aspas (simples ou duplas) 
+e números não levam aspas.*/
+
+// Declarar uma variável com o seu nome
+var nome = "Gabi";
+console.log(nome);
+
+// Declarar uma variável com a sua idade
+
+var idade = "29";
+console.log(idade);
+
+/*Declarar uma variável com a sua comida
+favorita e não atribuir valor*/
+
+var comidaFavorita;
+console.log(comidaFavorita);
+
+// Atribuir valor a sua comida favorita
+
+var comidaFavorita = "Castanha de Caju";
+console.log(comidaFavorita);
+
+// Declarar 5 variáveis diferentes sem valores
+var primeiraVar,
+    segundaVar,
+    terceiraVar,
+    quartaVar,
+    quintaVar;
