c70312e16d856a31be8e8a99abfcce68acfdbd8d
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-06 17:50:19 -0300

    Update Exercicio_Funcoes.js

--- 5.Funcoes/Exercicio_Funcoes.js
+++ 5.Funcoes/Exercicio_Funcoes.js
@@ -52,6 +52,7 @@ function nomeCompleto(nome,sobrenome){
   return nome + ' ' + sobrenome
 }
 
+//testando
 console.log(nome + ' ' + sobrenome);
 
 
@@ -60,16 +61,29 @@ console.log(nome + ' ' + sobrenome);
 
 // Crie uma função que verifica se um número é par
 
+num = 5
 
+function verificaPar(num){
+  if(num%2===0){
+    console.log('O número é par');
+  } else{
+    console.log('O número não é par');
+  }
+}
 
 
 
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
 
+valor = 'teste';
 
+function tipoDado(valor){
+  return (typeof valor)
+}
 
-
+//testando
+console.log(typeof valor);
 
 
 
@@ -78,9 +92,31 @@ console.log(nome + ' ' + sobrenome);
 // utilize essa função para mostrar no console o seu nome completo
 // quando o evento 'scroll' ocorrer.
 
+addEventListener('scroll', function() {
+  console.log('Gabriela Deutner');
+});
+
+
+
+
 // Corrija o erro abaixo
+
 function precisoVisitar(paisesVisitados) {
-    var totalPaises = 193;
+  var totalPaises = 193;
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+function jaVisitei(paisesVisitados) {
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+precisoVisitar(20);
+jaVisitei(20);
+
+
+
+//CORRIGIDO:
+var totalPaises = 193;
+
+function precisoVisitar(paisesVisitados) {
     return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
   }
   function jaVisitei(paisesVisitados) {
