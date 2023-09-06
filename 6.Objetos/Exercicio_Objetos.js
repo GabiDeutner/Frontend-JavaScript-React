7f87d86b907116e97f04fcc4464bbf9bb0f20ee1
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-06 17:58:11 -0300

    Create Exercicio_Objetos.js

---
+++ 6.Objetos/Exercicio_Objetos.js
@@ -0,0 +1,19 @@
+//EXERCÍCIO
+
+
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+    preco: 1000,
+    portas: 4,
+    marca: 'Audi',
+  }
+  
+  // Crie um objeto de um cachorro que represente um labrador,
+  // preto com 10 anos, que late ao ver um homem
+  
+  
