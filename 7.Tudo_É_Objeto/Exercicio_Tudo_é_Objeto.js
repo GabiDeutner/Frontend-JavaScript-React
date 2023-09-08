2c74152ead959084680a741964b13d311680e759
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-08 13:31:43 -0300

    Create Exercicio_Tudo_é_Objeto.js

---
+++ 7.Tudo_É_Objeto/Exercicio_Tudo_é_Objeto.js
@@ -0,0 +1,8 @@
+// EXERCÍCIO
+
+// nomeie 3 propriedades ou métodos de strings
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
