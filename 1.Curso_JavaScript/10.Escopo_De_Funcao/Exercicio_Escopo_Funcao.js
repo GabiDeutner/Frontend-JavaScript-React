6530af9ae0c109136c2ec2fffb01858356017c6b
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-12 10:21:23 -0300

    Update Exercicio_Escopo_Funcao.js

---
+++ 1.Curso_JavaScript/10.Escopo_De_Funcao/Exercicio_Escopo_Funcao.js
@@ -0,0 +1,29 @@
+// Por qual motivo o código abaixo retorna com erros?
+{
+    var cor = 'preto';
+    const marca = 'Fiat';
+    let portas = 4;
+  }
+  console.log(var, marca, portas);
+  
+  // Como corrigir o erro abaixo?
+  function somarDois(x) {
+    const dois = 2;
+    return x + dois;
+  }
+  function dividirDois(x) {
+    return x + dois;
+  }
+  somarDois(4);
+  dividirDois(6);
+  
+  // O que fazer para total retornar 500?
+  var numero = 50;
+  
+  for(var numero = 0; numero < 10; numero++) {
+    console.log(numero);
+  }
+  
+  const total = 10 * numero;
+  console.log(total);
+  
