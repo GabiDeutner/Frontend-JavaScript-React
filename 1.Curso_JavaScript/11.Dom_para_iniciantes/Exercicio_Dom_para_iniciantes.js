759faaf4d468f2efa77e289e2798061599b0cf2a
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-13 20:11:05 -0300

    Create Exercicio_Dom_para_iniciantes.js

---
+++ 1.Curso_JavaScript/11.Dom_para_iniciantes/Exercicio_Dom_para_iniciantes.js
@@ -0,0 +1,8 @@
+// Retorne o url da página atual utilizando o objeto window
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+// Retorne a linguagem do navegador
+
+// Retorne a largura da janela 
