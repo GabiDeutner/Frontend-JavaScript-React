82e95e86501176dbd2eb420905a9ab56b3420360
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-13 19:57:56 -0300

    Update Exercicio_Escopo_Funcao.js

--- 1.Curso_JavaScript/10.Escopo_De_Funcao/Exercicio_Escopo_Funcao.js
+++ 1.Curso_JavaScript/10.Escopo_De_Funcao/Exercicio_Escopo_Funcao.js
@@ -5,10 +5,24 @@
     let portas = 4;
   }
   console.log(var, marca, portas);
-  
-   // porque o var "vaza" o bloco, ou seja, quando temos um bloco o correto é usar const e let
-   // em vez de utilizar o var, porque fora do bloco o var retornará "undefined"
+   
+   // não faz sentido dar console.log no var, pois var não é uma variável e sim uma palavra chave
+   // o var "vaza" o bloco, ou seja, quando temos um bloco o correto é usar const e let dentro do bloco
+   // o certo é dar console.log no const e let dentro do bloco e não fora do bloco
+   // o var irá vazar e funcionar fora do bloco, mas não é o recomendado
+   // o certo é usar const e let com o console.log 
+   // dentro do bloco em vez de utilizar o var, porque fora do bloco o var retornará "undefined"
    //A partir de agora, devemos utilizar apenas const e let para declarmos variáveis.
+   
+   // Correção:
+
+   {
+    var cor = 'preto';
+    const marca = 'Fiat';
+    let portas = 4;
+    console.log(cor, marca, portas);
+  }
+
 
 
    
@@ -21,12 +35,42 @@
     return x + dois;
   }
   somarDois(4);
-  dividirDois(6);
-  
+  dividirDois(6); // o erro acontece porque o dois está dentro da função somarDois e fora da dividirDois
+  // o certo é declarar o dois fora de ambas as funções, assim as duas conseguem utilizar ele
+
+
+  // correção:
+
+  const dois = 2;
+  function somarDois(x) {
+    return x + dois;
+  }
+  function dividirDois(x) {
+    return x / dois;
+  }
+  console.log(somarDois(4));
+  console.log(dividirDois(6));
+
+
+
+
+
   // O que fazer para total retornar 500?
   var numero = 50;
   
   for(var numero = 0; numero < 10; numero++) {
+    console.log(numero);
+  }
+  
+  const total = 10 * numero;
+  console.log(total);
+  
+
+  //correção: Usar const e let
+
+  const numero = 50;
+  
+  for(let numero = 0; numero < 10; numero++) {
     console.log(numero);
   }
   
