113bcf73777a508784e8099b324bb9eabbd1becf
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2023-09-13 23:21:23 -0300

    Update Exercicio_Dom_para_iniciantes.js

--- 1.Curso_JavaScript/11.Dom_para_iniciantes/Exercicio_Dom_para_iniciantes.js
+++ 1.Curso_JavaScript/11.Dom_para_iniciantes/Exercicio_Dom_para_iniciantes.js
@@ -1,8 +1,19 @@
 // Retorne o url da página atual utilizando o objeto window
 
+const hrefpagina = window.location.href;
+console.log(hrefpagina);
+
 // Seleciona o primeiro elemento da página que
 // possua a classe ativo
 
+const elementoAtivo = document.querySelector('.ativo');
+
 // Retorne a linguagem do navegador
 
+const linguagem = window.navigator.language;
+console.log(language);
+
 // Retorne a largura da janela 
+
+const windowWidth = Window.innerWidth;
+console.log(windowWidth);
