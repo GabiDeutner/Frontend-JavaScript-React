6a400a239bc6e11c6a97e7c5d07930039e117a3e
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-19 09:04:09 -0300

    Update Exercicio_ForEach_ArrowFunction.js

--- 1.Curso_JavaScript/14.ForEach_ArrowFunction/Exercicio_ForEach_ArrowFunction.js
+++ 1.Curso_JavaScript/14.ForEach_ArrowFunction/Exercicio_ForEach_ArrowFunction.js
@@ -4,10 +4,30 @@
 
 // Mostre no console cada parágrado do site
 
+const paragrafos = document.querySelectorAll('p');
+
+paragrafos.forEach((item, index, array) => {
+    console.log(item);
+});
+
+
+
+
 // Mostre o texto dos parágrafos no console
 
+const paragrafos2 = document.querySelectorAll('p');
+
+paragrafos2.forEach((item) => {
+    console.log(item.innerText);
+  });
+
+
+
+
+
 // Como corrigir os erros abaixo:
-const imgs = document.querySelectorAll('img');
+
+/*const imgs = document.querySelectorAll('img');
 
 imgs.forEach(item, index => {
   console.log(item, index);
@@ -18,5 +38,22 @@ imgs.forEach( => {
   console.log(i++);
 });
 
+imgs.forEach(() => i++);*/
+
+// CORREÇÃO
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++);
+});
+
 imgs.forEach(() => i++);
+
+console.log(i);
 
