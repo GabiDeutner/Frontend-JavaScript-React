6cd6af2ccac7ade1b27119ec64dfc7d6352a5566
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-19 08:44:26 -0300

    Update Resumo_ForEach_ArrowFunction.js

--- 1.Curso_JavaScript/14.ForEach_ArrowFunction/Resumo_ForEach_ArrowFunction.js
+++ 1.Curso_JavaScript/14.ForEach_ArrowFunction/Resumo_ForEach_ArrowFunction.js
@@ -1,7 +1,8 @@
 //forEach e Arrow Function
 
 //FOREACH
-//Constantemente vamos selecionar uma lista de itens do dom. A melhor forma para interagirmos com os mesmos é utilizando o método forEach.
+//Constantemente vamos selecionar uma lista de itens do dom. 
+//A melhor forma para interagirmos com os mesmos é utilizando o método forEach.
 
 const imgs = document.querySelectorAll('img');
 
@@ -14,22 +15,25 @@ imgs.forEach(function(item){
 
 
 //PARÂMETROS DO FOREACH
-//O primeiro parâmetro é o callback, ou seja, a função que será ativada a cada item. Esse função pode receber três parâmetros: valorAtual, index e array;
+//O primeiro parâmetro é o callback, ou seja, a função que será ativada a cada item. 
+//Esse função pode receber três parâmetros: valorAtual, index e array;
 
-const imgs = document.querySelectorAll('img');
+const imgs2 = document.querySelectorAll('img');
 
-imgs.forEach(function(valorAtual, index, array){
+imgs2.forEach(function(valorAtual, index, array){
   console.log(item); // o item atual no loop
   console.log(index); // o número do index
-  console.log(array); // a Array completa
+  console.log(array); // a Array completo
 });
 
 
 
 
 
+
 //FOREACH E ARRAY
-//forEach é um método de Array, alguns objetos array-like possuem este método. Caso não possua, o ideal é transformá-los em uma array.
+//forEach é um método de Array, alguns objetos array-like possuem este método. 
+//Caso não possua, o ideal é transformá-los em uma array.
 
 const titulos = document.getElementsByClassName('titulo');
 const titulosArray = Array.from(titulos);
@@ -40,33 +44,38 @@ titulosArray.forEach(function(item){
 
 
 
+
+
 //ARROW FUNCTION
-//Sintaxe curta em relação a function expression. Basta remover a palavra chave function e adicionar a fat arrow => após os argumentos.
+//Sintaxe curta em relação a function expression. Basta remover a palavra chave function e 
+//adicionar a fat arrow => após os argumentos.
 
-const imgs = document.querySelectorAll('img');
+const imgs3 = document.querySelectorAll('img');
 
-imgs.forEach((item) => {
+imgs3.forEach((item) => {
   console.log(item);
 });
+
+
 
 
 
 //PARÂMETROS E PARÊNTESES
-const imgs = document.querySelectorAll('img');
+const imgs4 = document.querySelectorAll('img');
 
 // parâmetro único não precisa de parênteses
-imgs.forEach(item => {
+imgs4.forEach(item => {
   console.log(item);
 });
 
 // multiplos parâmetros precisam de parênteses
-imgs.forEach((item, index) => {
+imgs4.forEach((item, index) => {
   console.log(item, index);
 });
 
 // sem parâmetro precisa dos parênteses, mesmo vazio
 let i = 0;
-imgs.forEach(() => {
+imgs4.forEach(() => {
   console.log(i++);
 });
 
@@ -76,15 +85,16 @@ imgs.forEach(() => {
 
 
 
+
 //RETURN
 //É possível omitir as chaves {} para uma função que retorna uma linha.
 
-const imgs = document.querySelectorAll('img');
+const imgs5 = document.querySelectorAll('img');
 
-imgs.forEach(item => 
+imgs5.forEach(item => 
   console.log(item)
 );
 
-imgs.forEach(item => console.log(item));
+imgs5.forEach(item => console.log(item));
 
 //Não é permitido fechar a linha com ;
