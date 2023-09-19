176e7303bfc307e5ed10ad4fc24e0471497f60a1
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-19 11:38:31 -0300

    Create script.js

---
+++ 1.Curso_JavaScript/15.Classes_Atributos/script.js
@@ -0,0 +1,27 @@
+// Adicione a classe ativo a todos os itens do menu
+const itensMenu = document.querySelectorAll('.menu a');
+
+itensMenu.forEach((item) => {
+  item.classList.add('ativo');
+})
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+itensMenu.forEach((item) => {
+  item.classList.remove('ativo');
+});
+itensMenu[0].classList.add('ativo');
+
+// Verifique se as imagens possuem o atributo alt
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((img) => {
+  const possuiAtributo = img.hasAttribute('alt');
+  console.log(img, possuiAtributo);
+})
+
+
+// Modifique o href do link externo no menu
+const link = document.querySelector('a[href^="http"]');
+link.setAttribute('href', 'https://www.google.com/');
+
+console.log(link);
