5621e48782f2bb7d7c81207c95318b16a6536e4d
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-20 13:04:58 -0300

    Create Anotacoes.js

---
+++ 1.Curso_JavaScript/11.Dom_para_iniciantes/Anotacoes.js
@@ -0,0 +1,48 @@
+//Existem cinco formas de selecionar elementos do DOM:
+
+// 1. Por marca (tag) - seleciona mais de um objeto, porque seleciona todos com a mesma tag html
+getElementsByTagName();
+
+// 2. Por ID 
+getElementById();
+
+// 3. Por Nome
+getElementsByName();
+
+// 4. Por Classe
+getElementsByClassName();
+
+// 5. Por seletor (CSS)
+querySelector();
+querySelectorAll();
+
+/* DOM: Document Object Model
+Árvore DOM:
+
+window
+- location
+- document
+- history
+
+document
+- html
+
+html
+- head
+- body
+
+head
+- meta
+- title
+
+body
+- h1
+- p
+- div
+
+p
+- strong
+
+*/
+
+
