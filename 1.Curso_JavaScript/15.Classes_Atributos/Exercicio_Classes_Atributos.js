b2e167bf151c50ab7908da28afc05aeb56304de7
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-20 16:16:33 -0300

    Update Exercicio_Classes_Atributos.js

--- 1.Curso_JavaScript/15.Classes_Atributos/Exercicio_Classes_Atributos.js
+++ 1.Curso_JavaScript/15.Classes_Atributos/Exercicio_Classes_Atributos.js
@@ -12,13 +12,33 @@ itensMenu.forEach((item) => {
   })
 
 
+
+
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+
+itensMenu.forEach((item) => {
+  item.classList.remove('ativo');
+});
+itensMenu[0].classList.add('ativo');
+
 
 
 
 // Verifique se as imagens possuem o atributo alt
 
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((img) => {
+  const possuiAtributo = img.hasAttribute('alt');
+  console.log(img, possuiAtributo);
+})
+
+
 
 
 // Modifique o href do link externo no menu
 
+const link = document.querySelector('a[href^="http"]');
+link.setAttribute('href', 'https://www.google.com/');
+
+console.log(link);
