3b009923b118bab2d01b3e243fa42a1825b33d65
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-22 09:32:38 -0300

    Update Exercicio_Traversing_Manipulacao.js

--- 1.Curso_JavaScript/18.Traversing_Manipulacao/Exercicio_Traversing_Manipulacao.js
+++ 1.Curso_JavaScript/18.Traversing_Manipulacao/Exercicio_Traversing_Manipulacao.js
@@ -2,8 +2,33 @@
 
 // Duplique o menu e adicione ele em copy
 
+const menu = document.querySelector('.menu'); // selecionei o menu
+const copy = document.querySelector('.copy'); // selecioonei a copy
+
+const cloneMenu = menu.cloneNode(true); // clonei o menu e os filhos dele
+copy.appendChild(cloneMenu); // adicionei o clone do menu na copy
+
+
+
+
 // Selecione o primeiro DT da dl de Faq
+
+const faq = document.querySelector('.faq'); // selecionei a faq
+const primeiroDt = faq.querySelector('dt'); // dentro da faq eu selecionei o dt 
+// a querySelector por default sempre seleciona o primeiro elemento
+
+
+
 
 // Selecione o DD referente ao primeiro DT
 
+const proximoDD = primeiroDt.nextElementSibling; // Dt e DD são irmãos (estão no mesmo nível)
+// então seleciona o próximo irmão de DT que será um DD
+console.log(proximoDD);
+
+
+
 // Substitua o conteúdo html de .faq pelo de .animais
+
+const animais = document.querySelector('.animais'); // selecionei animais
+faq.innerHTML = animais.innerHTML; // o HTML de faq recebe o HTML de animais
