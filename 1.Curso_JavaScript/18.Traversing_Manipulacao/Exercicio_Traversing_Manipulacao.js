03cf411b32335c382f321714f3c0836c91cf0284
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-22 08:23:41 -0300

    Create Exercicio_Traversing_Manipulacao.js

---
+++ 1.Curso_JavaScript/18.Traversing_Manipulacao/Exercicio_Traversing_Manipulacao.js
@@ -0,0 +1,9 @@
+// EXERCÍCIO
+
+// Duplique o menu e adicione ele em copy
+
+// Selecione o primeiro DT da dl de Faq
+
+// Selecione o DD referente ao primeiro DT
+
+// Substitua o conteúdo html de .faq pelo de .animais
