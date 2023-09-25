611f77a2768dc542e6447daccfbefdab20a43e11
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-25 18:57:42 -0300

    Create Exercicio_Constructor_Functions.js

---
+++ 1.Curso_JavaScript/23.Constructor_Functions/Exercicio_Constructor_Functions.js
@@ -0,0 +1,24 @@
+// EXERCÍCIOS
+
+// Transforme o objeto abaixo em uma Constructor Function
+const pessoa = {
+    nome: 'Nome pessoa',
+    idade: 0,
+    andar() {
+      console.log(this.nome + ' andou');
+    }
+  }
+  
+  // Crie 3 pessoas, João - 20 anos,
+  // Maria - 25 anos, Bruno - 15 anos
+  
+  
+  // Crie uma Constructor Function (Dom) para manipulação
+  // de listas de elementos do dom. Deve conter as seguintes
+  // propriedades e métodos:
+  //
+  // elements, retorna NodeList com os elementos selecionados
+  // addClass(classe), adiciona a classe a todos os elementos
+  // removeClass(classe), remove a classe a todos os elementos
+  
+  
