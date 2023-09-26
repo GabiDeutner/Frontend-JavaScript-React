c203fc25ca4d2fe1e366db7c5cd488f6a55dd6dc
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-26 11:34:38 -0300

    Create Exercicio_Prototype.js

---
+++ 1.Curso_JavaScript/24.Prototype/Exercicio_Prototype.js
@@ -0,0 +1,24 @@
+// Exercícios
+
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+
+li;
+li.click;
+li.innerText;
+li.value;
+li.hidden;
+li.offsetLeft;
+li.click();
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name;
