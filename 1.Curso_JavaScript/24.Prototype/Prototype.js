56ff1a6242b10434e096fc91f8e1ca9230e4d067
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-26 16:25:22 -0300

    Update Prototype.js

--- 1.Curso_JavaScript/24.Prototype/Prototype.js
+++ 1.Curso_JavaScript/24.Prototype/Prototype.js
@@ -79,3 +79,96 @@
     objetos do JavaScript e são usados para criar hierarquias de objetos e compartilhar 
     comportamentos comuns entre eles.
 */
+
+
+
+/*
+    O conceito de prototype em JavaScript é fundamental para entender a herança e o 
+    compartilhamento de propriedades e métodos entre objetos. 
+    Vou explicar cada parte do código passo a passo:
+
+    Definindo uma Função Construtora:
+*/
+
+    function Pessoa(nome, idade) {
+      this.nome = nome;
+      this.idade = idade;
+    }
+
+
+    /*
+
+    Neste trecho, você está definindo uma função construtora chamada Pessoa. 
+    Esta função será usada para criar novos objetos do tipo "Pessoa" com propriedades nome e idade.
+
+    Acessando o prototype da Função Construtora:
+
+    */
+
+    console.log(Pessoa.prototype); // retorna o objeto
+
+
+    /*
+    Aqui, você está acessando o prototype da função Pessoa. O prototype de uma função 
+    construtora é um objeto especial que será usado como o protótipo de todos os objetos 
+    criados por essa função. É onde você pode adicionar propriedades e métodos que serão 
+    compartilhados por todas as instâncias criadas a partir dessa função.
+
+    Criando uma Instância:
+    */
+
+
+    const andre = new Pessoa('André', 28);
+
+
+    /*
+    Neste trecho, você está criando uma nova instância da função construtora Pessoa e 
+    atribuindo-a à variável andre. Esta instância terá suas próprias propriedades nome e 
+    idade específicas.
+
+    Acessando o prototype de uma Instância:
+    */
+
+
+    console.log(andre.prototype); // undefined
+
+
+    /*
+    Aqui, você está tentando acessar o prototype da instância andre. No entanto, as 
+    instâncias não têm uma propriedade prototype diretamente. Em vez disso, elas herdam 
+    as propriedades e métodos do prototype da função construtora que as criou.
+
+    Adicionando Métodos ao prototype da Função Construtora:
+    */
+
+
+    Pessoa.prototype.andar = function() {
+      return this.nome + ' andou';
+    }
+    Pessoa.prototype.nadar = function() {
+      return this.nome + ' nadou';
+    }
+
+    
+    /*
+    Nesses trechos, você está adicionando métodos (andar e nadar) ao prototype da função 
+    construtora Pessoa. Isso significa que todas as instâncias criadas a partir de Pessoa 
+    terão acesso a esses métodos, pois eles são compartilhados por todas as instâncias.
+
+    Acessando o prototype Atualizado da Função Construtora:
+    */
+
+    console.log(Pessoa.prototype); // retorna o objeto com os novos métodos
+
+
+    /*
+    Aqui, você está acessando o prototype da função construtora Pessoa após adicionar os novos 
+    métodos. Agora, o prototype contém os métodos andar e nadar, que podem ser compartilhados 
+    por todas as instâncias criadas a partir de Pessoa.
+
+    Em resumo, o prototype em JavaScript permite que você compartilhe propriedades e métodos 
+    entre objetos criados por uma função construtora. É uma parte fundamental da herança de 
+    objetos em JavaScript e é usado para economizar memória, uma vez que os métodos compartilhados 
+    não precisam ser recriados para cada instância separadamente.
+
+    */
