92fbeb1b489d238e7cbc1a175505329426383221
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-26 09:48:56 -0300

    Update Exercicio_Constructor_Functions.js

--- 1.Curso_JavaScript/23.Constructor_Functions/Exercicio_Constructor_Functions.js
+++ 1.Curso_JavaScript/23.Constructor_Functions/Exercicio_Constructor_Functions.js
@@ -1,18 +1,31 @@
 // EXERCÍCIOS
 
 // Transforme o objeto abaixo em uma Constructor Function
-const pessoa = {
+/* const pessoa = {
     nome: 'Nome pessoa',
     idade: 0,
     andar() {
       console.log(this.nome + ' andou');
     }
-  }
+  } */
   
+  function Pessoa(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+    this.andar = function() {
+      console.log(nome + ' Andou');
+    }
+  }
+
   // Crie 3 pessoas, João - 20 anos,
   // Maria - 25 anos, Bruno - 15 anos
   
-  
+  const joao = new Pessoa('João', 20);
+  const maria = new Pessoa('Maria', 25);
+  const bruno = new Pessoa('Bruno', 15);
+
+
+
   // Crie uma Constructor Function (Dom) para manipulação
   // de listas de elementos do dom. Deve conter as seguintes
   // propriedades e métodos:
@@ -21,4 +34,21 @@ const pessoa = {
   // addClass(classe), adiciona a classe a todos os elementos
   // removeClass(classe), remove a classe a todos os elementos
   
+
+  function Dom(seletor) {
+    const elementList = document.querySelectorAll(seletor);
+    this.elements = elementList;
+    this.addClass = function(classe) {
+      elementList.forEach((element) => {
+        element.classList.add(classe);
+      })
+    }
+    this.removeClass = function(classe) {
+      elementList.forEach((element) => {
+        element.classList.remove(classe);
+      })
+    }
+  }
   
+  const listaItens = new Dom('li');
+  const ul = new Dom('ul');
