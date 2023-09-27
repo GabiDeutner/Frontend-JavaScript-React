2d8f3d4701e406fe35f4ffb1feaa4d01fd5e564a
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-27 19:30:23 -0300

    Update Exercicio_Prototype.js

--- 1.Curso_JavaScript/24.Prototype/Exercicio_Prototype.js
+++ 1.Curso_JavaScript/24.Prototype/Exercicio_Prototype.js
@@ -5,20 +5,38 @@
 // Crie um método no protótipo que retorne
 // o nome completo da pessoa
 
+function Pessoa(nome, sobrenome, idade) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  } 
+  
+  Pessoa.prototype.nomeCompleto = function() {
+    return `${this.nome} ${this.sobrenome}`
+  }
+  
+  const andre = new Pessoa('André', 'Rafael', 28);
+
+
 // Liste os métodos acessados por 
 // dados criados com NodeList,
 // HTMLCollection, Document
 
+Object.getOwnPropertyNames(NodeList.prototype)
+Object.getOwnPropertyNames(HTMLCollection.prototype)
+Object.getOwnPropertyNames(Document.prototype)
+
+
 // Liste os construtores dos dados abaixo
 const li = document.querySelector('li');
 
-li;
-li.click;
-li.innerText;
-li.value;
-li.hidden;
-li.offsetLeft;
-li.click();
+li; // HTMLLIElement
+li.click; // Function
+li.innerText; // String
+li.value; // Number
+li.hidden; // Boolean
+li.offsetLeft; // Number
+li.click(); // undefined
 
 // Qual o construtor do dado abaixo:
-li.hidden.constructor.name;
+li.hidden.constructor.name; // String
