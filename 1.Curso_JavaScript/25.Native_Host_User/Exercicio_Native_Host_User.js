f74cdde239f2a83a75ebdce4669f23baada9a868
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-28 11:47:46 -0300

    Update Exercicio_Native_Host_User.js

--- 1.Curso_JavaScript/25.Native_Host_User/Exercicio_Native_Host_User.js
+++ 1.Curso_JavaScript/25.Native_Host_User/Exercicio_Native_Host_User.js
@@ -1,8 +1,24 @@
 // EXERCICIOS
 
 // Liste 5 objetos nativos
+Object
+String
+Array
+Function
+Number
 
 // Liste 5 objetos do browser
+Window
+history
+Document
+HTMLCollection
+NodeList
 
 // Liste 2 Métodos, Propriedades ou Objetos
 // presentes no Chrome que não existem no Firefox
+
+if (typeof document.webkitVisibilityState !== "undefined" ) {
+  console.log('Existe');
+} else {
+  console.log('Não existe');
+}
