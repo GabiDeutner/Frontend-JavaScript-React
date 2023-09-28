399a994b981f99057222a4d5d7c4338f8d7868e2
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-09-28 16:36:42 -0300

    Create Exercicio_String.js

---
+++ 1.Curso_JavaScript/26.String/Exercicio_String.js
@@ -0,0 +1,46 @@
+// EXERCÍCIOS
+
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+
+const transacoes = [
+    {
+      descricao: 'Taxa do Pão',
+      valor: 'R$ 39',
+    },
+    {
+      descricao: 'Taxa do Mercado',
+      valor: 'R$ 129',
+    },
+    {
+      descricao: 'Recebimento de Cliente',
+      valor: 'R$ 99',
+    },
+    {
+      descricao: 'Taxa do Banco',
+      valor: 'R$ 129',
+    },
+    {
+      descricao: 'Recebimento de Cliente',
+      valor: 'R$ 49',
+    },
+  ];
+  
+  // Retorne uma array com a lista abaixo
+  const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+  
+  // Substitua todos os span's por a's
+  const html = `<ul>
+                  <li><span>Sobre</span></li>
+                  <li><span>Produtos</span></li>
+                  <li><span>Contato</span></li>
+                </ul>`;
+  
+  // Retorne o último caracter da frase
+  const frase = 'Melhor do ano!';
+  
+  // Retorne o total de taxas
+  const transacoes = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+  
+  
+  
