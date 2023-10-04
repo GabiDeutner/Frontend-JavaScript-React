05c612891e73e5ea77bc823677314d6edc77a8e1
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-10-04 16:38:59 -0300

    Create Primeiro.jsx

---
+++ Anotacoes_React/Exemplo de projeto em React/Components/Primeiro.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+/* function Primeiro() {
+	return (
+		<div>
+			<h1>Primeiro Componente</h1>
+			<h2>Exemplo de um componente React</h2>
+		</div>
+
+	)
+}
+
+export default Primeiro */
+
+
+export default () =>
+		<>
+			<h1>Primeiro Componente</h1>
+			<h2>Exemplo de um componente React</h2>
+		</>
