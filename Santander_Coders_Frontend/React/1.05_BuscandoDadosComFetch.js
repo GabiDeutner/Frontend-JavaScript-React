1ed20630bb9e80c459247afcd55550460a373ad8
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-11-27 15:50:44 -0300

    Buscando Dados com Fetch

---
+++ Santander_Coders_Frontend/React/1.05_BuscandoDadosComFetch.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+
+    const tarefas = [
+        { id: '1', title: 'minha primeira tarefa'},
+        { id: '2', title: 'minha segunda tarefa'},
+        { id: '3', title: 'minha terceira tarefa'},
+        { id: '4', title: 'minha quarta tarefa'}
+    ]
+
+export default function App(){
+
+    const [tarefas, setTarefas] = useState([])
+
+    useEffect(() => {
+
+        async function buscarDados(){
+            const resultado = await fetch('https://jsonplaceholder.typicode.com/todos')
+            const resultadoFinal = await resultado.json()
+            return resultadoFinal
+        }
+
+        buscarDados().then(res => console.log(res))
+
+    }, [])
+
+    return (
+        <div>
+            <h1>Buscando dados</h1>
+            <ol>
+            {tarefas.map((tarefa) => {
+             return (
+                <div>
+                <li key={tarefa.id}>
+                {tarefa.title}
+                {tarefa.completed ? '- Tarefa Concluída' : null}
+                </li>
+                </div>
+             )
+            })}
+            </ol>
+        </div>
+    )
+}
