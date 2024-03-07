8cf8a6afda12dc4557469914974284cee8c927dd
Gabriela Paulini Deutner <deutnerg@gmail.com>
2024-03-07 17:17:12 -0300

    estudando javascript

---
+++ 1.Curso_JavaScript/1.05 Funcoes/brincando-com-functions.js
@@ -0,0 +1,75 @@
+let soma = 0
+
+
+function Somar (n1, n2){
+  soma = n1 + n2
+  return console.log(soma)
+}
+
+Somar(1,1)
+
+
+
+function testeVazia (){
+  return console.log('testando função')
+}
+
+testeVazia ()
+
+
+function PassandoUmParametro (param){
+  return console.log(param)
+}
+
+PassandoUmParametro(3)
+
+
+
+
+function AreaDoQuadrado (lado){
+  return lado * lado
+}
+
+console.log(AreaDoQuadrado(2))
+
+
+function pi (){
+  return 3.14
+}
+
+console.log(pi())
+
+let testePi = 5 * pi()
+
+console.log(testePi)
+
+
+function CalculaIMC (peso, altura){
+    imc = peso / (altura ** 2)
+    return imc
+  }
+  
+  console.log(CalculaIMC(52,1.56))
+
+
+  function CorFavorita (cor){
+    if (cor === 'verde'){
+      return console.log('você curte verde')
+    } else if  (cor === 'azul'){
+      return console.log('tu curtes azul')
+    } else{
+      return console.log('tu precisas gostar de alguma cor!')
+    }
+  }
+  
+  CorFavorita('verde')
+
+
+  addEventListener('click', function(){
+    console.log('clicou')
+  }
+  
+  );
+
+
+  
