40a33e0dae1e196ff67959c5e69688b636fd15c7
Gabriela Paulini Deutner <deutnerg@gmail.com>
2024-03-08 17:58:55 -0300

    estudando tudo é objeto

---
+++ 1.Curso_JavaScript/1.07 Tudo_É_Objeto/treinando-objetos.js
@@ -0,0 +1,26 @@
+const nome = 'Gabis';
+
+const tamanho = nome.length;
+console.log(tamanho);
+
+const letrinha = nome.charAt(1);
+console.log(letrinha);
+
+const letrinhaz = nome.charAt(2);
+console.log(letrinhaz);
+
+const substituir = nome.replace('bis','bizz');
+console.log(substituir);
+console.log(nome);
+
+
+
+
+
+
+
+
+
+
+
+
